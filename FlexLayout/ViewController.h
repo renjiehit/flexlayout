8e865a3454967c83dcac67e46d7f1f81d6a2d083
任洪杰 <renhongjie@yidian-inc.com>
2018-09-03 11:45:25 +0800

    Initial Commit

---
+++ FlexLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlexLayout
+//
+//  Created by 任 洪杰 on 2018/9/3.
+//  Copyright © 2018年 任 洪杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlexLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlexLayout
+//
+//  Created by 任 洪杰 on 2018/9/3.
+//  Copyright © 2018年 任 洪杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
