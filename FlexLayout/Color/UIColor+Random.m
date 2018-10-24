b316c738ad800d6c5f2179d39ecdf504a07cfec3
任洪杰 <renhongjie@yidian-inc.com>
2018-10-24 10:40:02 +0800

    optimize random color

--- FlexLayout/Color/UIColor+Random.m
+++ FlexLayout/Color/UIColor+Random.m
@@ -7,7 +7,8 @@
 //
 
 #import "UIColor+Random.h"
-static const NSString *colorValues = @"#fe4a49,#2ab7ca,#fed766,#e6e6ea,#f4f4f8,#eee3e7,#ead5dc,#eec9d2,#f4b6c2,#f6abb6,#011f4b,#03396c,#005b96,#6497b1,#b3cde0,#051e3e,#251e3e,#451e3e,#651e3e,#851e3e,#dec3c3,#e7d3d3,#f0e4e4,#f9f4f4,#4a4e4d,#0e9aa7,#3da4ab,#f6cd61,#fe8a71,#2a4d69,#4b86b4,#adcbe3,#e7eff6,#63ace5,#fe9c8f,#feb2a8,#fec8c1,#fad9c1,#f9caa7,#009688,#35a79c,#54b2a9,#65c3ba,#83d0c9,#ee4035,#f37736,#fdf498,#7bc043,#0392cf,#d0e1f9,#4d648d,#283655,#1e1f26,#96ceb4,#ffeead,#ff6f69,#ffcc5c,#88d8b0,#a8e6cf,#dcedc1,#ffd3b6,#ffaaa5,#ff8b94,#d11141,#00b159,#00aedb,#f37735,#ffc425,#ebf4f6,#bdeaee,#76b4bd,#58668b,#5e5656,#ff77aa,#ff99cc,#ffbbee,#ff5588,#ff3377,#fff6e9,#ffefd7,#fffef9,#e3f0ff,#d2e7ff,#84c1ff,#add6ff,#d6eaff,#eaf4ff,#f8fbff,#bfd6f6,#8dbdff,#64a1f4,#4a91f2,#3b7dd8";
+
+static const NSString *colorValues = @"#fe4a49,#2ab7ca,#fed766,#e6e6ea,#f4f4f8,#eee3e7,#ead5dc,#eec9d2,#f4b6c2,#f6abb6,#005b96,#6497b1,#b3cde0,#dec3c3,#e7d3d3,#f0e4e4,#f9f4f4,#0e9aa7,#3da4ab,#f6cd61,#fe8a71,#4b86b4,#adcbe3,#e7eff6,#63ace5,#fe9c8f,#feb2a8,#fec8c1,#fad9c1,#f9caa7,#35a79c,#54b2a9,#65c3ba,#83d0c9,#f37736,#fdf498,#7bc043,#0392cf,#eeeeee,#dddddd,#cccccc,#bbbbbb,#fce9db,#e0a899,#dfa290,#96ceb4,#ffeead,#ff6f69,#ffcc5c,#88d8b0,#d2d4dc,#afafaf,#f8f8fa,#e5e6eb,#c0c2ce,#a8e6cf,#dcedc1,#ffd3b6,#ffaaa5,#ff8b94,#ebf4f6,#bdeaee,#76b4bd,#ff77aa,#ff99cc,#ffbbee,#ff5588,#fff6e9,#ffefd7,#fffef9,#e3f0ff,#d2e7ff,#84c1ff,#add6ff,#d6eaff,#eaf4ff,#bfd6f6,#8dbdff,#64a1f4,#4a91f2,#e3c9c9,#f4e7e7,#eedbdb,#cecbcb,#cbdadb";
 
 @implementation UIColor (Random)
 
